a943d87643c911060f37870f716e0fbfd624af19
Mustafa Har <91052635+MustafaHar@users.noreply.github.com>
2023-01-06 14:14:42 +0100

    Update installPrerequis.sh

--- installPrerequis.sh
+++ installPrerequis.sh
@@ -1,4 +1,5 @@
 # Installation AZ CLI
+cd ..
 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
@@ -20,3 +21,4 @@ wget https://github.com/kubernetes-sigs/kustomize/releases/download/v3.2.0/kusto
 
 curl --silent --location --remote-name "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.2.3/kustomize_kustomize.v3.2.3_linux_amd64" && chmod a+x kustomize_kustomize.v3.2.3_linux_amd64 && sudo mv kustomize_kustomize.v3.2.3_linux_amd64 /usr/local/bin/kustomize
 
+cd test
