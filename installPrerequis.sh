c9d3bdd944652de3d55effd3f37b6e2b633d7db6
Mustafa Har <91052635+MustafaHar@users.noreply.github.com>
2023-01-06 14:59:48 +0100

    Update installPrerequis.sh

--- installPrerequis.sh
+++ installPrerequis.sh
@@ -6,13 +6,15 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Installation de kubectl
 
-curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-curl -LO https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum –-check
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+kubectl version --client
 
 
 # Installation de Kustomize v3.2.0
